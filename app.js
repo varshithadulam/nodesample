c1a538ef35b2a83790fdf16bf3eb6babb6983a8d
varshithadulam <dulamvarshitha9@gmail.com>
2024-10-22 15:44:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 app.get('/',(req,res) => {
 res.send("Hello World, I am from Nodejs");
 });
-app.listen(3020,() => {
-console.log("Server is running on port 3020")
+app.listen(3070,() => {
+console.log("Server is running on port 3070")
 });
